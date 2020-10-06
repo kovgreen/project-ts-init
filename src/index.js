e1db5345e38f90cf43318728c9398c803fa44ec1
Oleh Koval <kovgreen@gmail.com>
2020-10-06 21:50:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from "./redux/store";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App title="Props title" />
+      <App/>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
